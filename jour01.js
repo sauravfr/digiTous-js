c497fa0986f722b735a9b54d941df4c88dfcb5d1
Emma Paris <eparis@atlassian.com>
2020-12-07 13:24:21 +0100

    Concatenation

--- jour01.js
+++ jour01.js
@@ -2,8 +2,13 @@
 
 console.log("Hello World !");
 
-//02 - String
+//02-String and 
 
 var myName = "Saurav";
 
+console.log(myName);
+
+//03-Concatenation
+
 console.log("My name is " + myName +".");
+
