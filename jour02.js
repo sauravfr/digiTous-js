7a422d66230d57995f82caa839be1f57cf5358ad
Emma Paris <eparis@atlassian.com>
2020-12-08 16:07:34 +0100

    add Bonus II

--- jour02.js
+++ jour02.js
@@ -70,3 +70,24 @@ if(random===6){
 }else{
     console.log("So close...");
 }
+
+//Bonus II
+var month = "January";
+switch (month) {
+	case "January": 
+		console.log("Winter");
+		break;
+	case "March": 
+		console.log( "Spring");
+		break;
+	case "June": 
+		console.log("Summer");
+		break;
+	case "September": 
+		console.log("Fall");
+		break;
+	default: 
+		console.log("That's not a season...");
+}
+
+ 
