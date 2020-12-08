0c3cec42eacc74ed0d696270d66b10499092f8ee
Emma Paris <eparis@atlassian.com>
2020-12-08 14:28:27 +0100

    add jour02 js file

---
+++ jour02.js
@@ -0,0 +1,33 @@
+//01 - Number
+var integer = 102;
+
+var float = 13.9;
+
+console.log(integer);
+
+console.log(float);
+
+//02 - Convert
+
+var basic = 34;
+
+var stringified = basic.toString();
+
+console.log(stringified);
+
+//03 - Round
+
+var num = 1.5;
+
+var rounded = Math.round(1.5);
+
+console.log(rounded);
+
+//04 - Arithmétique
+
+var test = 12;
+
+var bis = 5;
+
+console.log(test+bis);
+
