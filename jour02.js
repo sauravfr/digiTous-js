3f588cf1f7eb7453198b2d994552fb3200fabf4e
Emma Paris <eparis@atlassian.com>
2020-12-08 15:21:25 +0100

    Condition III

--- jour02.js
+++ jour02.js
@@ -1,46 +1,31 @@
 //01 - Number
 var integer = 102;
-
 var float = 13.9;
-
 console.log(integer);
-
 console.log(float);
 
 //02 - Convert
-
 var basic = 34;
-
 var stringified = basic.toString();
-
 console.log(stringified);
 
 //03 - Round
-
 var num = 1.5;
-
 var rounded = Math.round(1.5);
-
 console.log(rounded);
 
 //04 - Arithmétique
 
 var test = 12;
-
 var bis = 5;
-
 console.log(test+bis);
 
 //05 - Comparaison
-
 var test =143;
-
 var bis = 219;
-
 console.log(test==bis);
 
 //06 - Condition
-
 var limit = 50;
 var score = 64;
 if(limit<=score){
@@ -50,11 +35,17 @@ if(limit<=score){
 }
 
 //07 - Condition II
-
 var password = "azerty";
-
 if(password.length>=5){
     console.log("The password is secure");
 }
 
-console.log(password.length);
+//08 - Condition III
+
+if(limit<=score || password.length>=5) {
+    console.log( "Everything is good");
+}else if(limit<=score && password.length>=5){
+    console.log("Something is good");
+}else{
+    console.log("Nothing is good" );
+}
