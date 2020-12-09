54a5d3f3d37fa521cacaf19c24e8b8402fef5877
Emma Paris <eparis@atlassian.com>
2020-12-09 14:17:19 +0100

    Add 02-Acces

--- jour03.js
+++ jour03.js
@@ -3,3 +3,8 @@ var fruits = ["mango", "lemon","blueberry"];
 console.log("Array list:", fruits);
 console.log("Array table:")
 console.table(fruits);
+
+//02 - Access
+var ingredients = ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf("butter"));
