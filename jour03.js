2a1416a9752ed41bc58576902dad4caf16e38e83
Emma Paris <eparis@atlassian.com>
2020-12-09 20:51:41 +0100

    add Bonus III

--- jour03.js
+++ jour03.js
@@ -69,3 +69,11 @@ while(i<=10){
     total+=i;
 };
 console.log(total);
+
+//Bonus III
+var promo4 = ["Ahmed", "Mohammed", "Houra", "Mohamad Bailo", "Kitty", "Elisabeth", "Nicolas", "Yeshi Tsering", "Mahmoudou","Allan", "Ewa",
+"Mosindo", "Hassan", "Sébastien", "Damir", "Hadama", "Thi", "Mohamed", "Atif", "Saurav"];
+
+var name = Math.floor(Math.random() * promo4.length);
+
+console.log(promo4[name]);
---
+++ tast.js
@@ -0,0 +1,6 @@
+var promo4 = ["Ahmed", "Mohammed", "Houra", "Mohamad Bailo", "Kitty", "Elisabeth", "Nicolas", "Yeshi Tsering", "Mahmoudou","Allan", "Ewa",
+"Mosindo", "Hassan", "Sébastien", "Damir", "Hadama", "Thi", "Mohamed", "Atif", "Saurav"];
+
+var name = Math.floor(Math.random() * promo4.length);
+
+console.log(promo4[name]);
