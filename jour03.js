5eef8f8a3616c9c32ac575f8c016a104de493cb3
Emma Paris <eparis@atlassian.com>
2020-12-09 15:24:26 +0100

    Add 03-add and remove

--- jour03.js
+++ jour03.js
@@ -8,3 +8,20 @@ console.table(fruits);
 var ingredients = ["eggs", "milk", "butter"];
 console.log(ingredients[1]);
 console.log(ingredients.indexOf("butter"));
+
+//03 - Add and Remove
+/*var objects = ["pen", "book", "lamp"];
+console.log(objects.push("chair"));
+console.log(objects);*/
+var objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+console.log(objects);
+objects.pop("lamp");
+console.log(objects);
+objects.push("laptop");
+console.log(objects);
+objects.shift("chair");
+console.log(objects);
+
+
+
