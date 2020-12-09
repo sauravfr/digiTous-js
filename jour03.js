23ca17e3784287b998869624d1f59b40eb549abf
Emma Paris <eparis@atlassian.com>
2020-12-09 18:36:04 +0100

    Add Bonus

--- jour03.js
+++ jour03.js
@@ -46,3 +46,18 @@ for (var i = sentence.length - 1; i >= 0; i--) {
     newString = newString += sentence[i]; 
 }
 console.log(newString);
+
+//Bonus
+
+for (var i=1; i <= 100; i++){
+    if (i % 3 ==0)
+        console.log("fizz");
+    else if (i% 5 ==0)
+        console.log("buzz");
+    else if (i % 3 ==0 && i * 5 ==0)
+        console.log("fizzbuzz");
+    else if (i % 7 ==0)
+    console.log("fizzbuzz");    
+    else
+        console.log(i);
+};
