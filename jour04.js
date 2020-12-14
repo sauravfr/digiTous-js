8fc8dba0c03babefbe0af6266dc18a9d054b307c
Emma Paris <eparis@atlassian.com>
2020-12-14 13:58:32 +0100

    Add 02-Combine

--- jour04.js
+++ jour04.js
@@ -8,6 +8,22 @@ var cat = {
 
 console.log(cat);
 console.log(cat.age);
-if (cat.isCute == true) {
+if (cat.isCute === true) {
+    console.log("So cute !");
+}
+
+//02 - Combine
+
+var cat2 = {
+    name: "Tiger",
+    age: 4,
+    isCute: true
+}
+
+var cats = [cat, cat2];
+console.log(cats);
+console.log(cat2.age);
+console.table(cats);
+if (cat2.isCute == true) {
     console.log("So cute !");
 }
