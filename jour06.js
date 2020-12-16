b77c542fcba6cfff1267ec086d1dbbaf522ec11a
Emma Paris <eparis@atlassian.com>
2020-12-16 16:01:28 +0100

    update

--- jour06.js
+++ jour06.js
@@ -1,8 +1,6 @@
 //01 - Format
 function formatDate(aaaa, mm, jj) {
-    var year = new Date(aaaa);
-    var month = new Date(mm);
-    var day = new Date(jj);
-    console.log(`${jj}/${mm}/${aaaa}`);
+
+    console.log(`${jj}/${mm}/${aaaa}`)
 }
-formatDate(2020, 12, 16);
+formatDate(2020, 12, 16)
