422283cedb28c91cfb318dcc399340f607f2e03d
Emma Paris <eparis@atlassian.com>
2020-12-17 16:56:33 +0100

    Add 04-Swap

--- jour07.js
+++ jour07.js
@@ -13,3 +13,21 @@ function countEach(string) {
     console.log(regex.test(string));
 }
 countEach("xxxoooox");
+
+//04 - Swap
+
+function swap(text) {
+    var helloWorld = ''
+    var string;
+    for (i = 0; i <= text.length - 1; i++) {
+        string = text[i];
+        if (text[i] == string.toUpperCase()) {
+            helloWorld += string.toLowerCase();
+        } else {
+            helloWorld += string.toUpperCase();
+        }
+    }
+    console.log(helloWorld)
+}
+
+swap("Hello World");
