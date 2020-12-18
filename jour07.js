b64ea532217603bd7ceb87c3d2fdcb77ae563280
Emma Paris <eparis@atlassian.com>
2020-12-18 11:31:07 +0100

    update jour07

--- jour07.js
+++ jour07.js
@@ -7,10 +7,34 @@ function sortLetters(text) {
 }
 sortLetters("konexio");
 
-//02 - XOXO
+/*//02 - XOXO
 function countEach(string) {
     var regex = /[a-z]+/g;
     console.log(regex.test(string));
+}
+countEach("xxxoooox");*/
+//Exo 02
+function countEach(string) {
+    var countX = 0;
+    var countO = 0;
+    for (var i = 0; i < string.length; i++) {
+        var letter = string.charAt(i);
+        if (letter === "x") {
+            countX++
+        }
+        if (letter === "o") {
+            countO++
+        }
+        console.log(letter);
+    }
+
+    console.log(countX)
+    console.log(countO)
+    if (countX === countO) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
 }
 countEach("xxxoooox");
 
--- test2.js
+++ test2.js
@@ -1,11 +1,24 @@
-//03 - Palindrome
-var checkPal = function(string) {
-    var str = string.split('').reverse().join('')
-    if (string = str) {
-        return "Palindrome !";
+//Exo 02
+function countEach(string) {
+    var countX = 0;
+    var countO = 0;
+    for (var i = 0; i < string.length; i++) {
+        var letter = string.charAt(i);
+        if (letter === "x") {
+            countX++
+        }
+        if (letter === "o") {
+            countO++
+        }
+        console.log(letter);
+    }
+
+    console.log(countX)
+    console.log(countO)
+    if (countX === countO) {
+        console.log(true)
     } else {
-        return "Nope";
+        console.log(false)
     }
 }
-var check = checkPal("racecar");
-console.log(check);
+countEach("xxxoooox");
