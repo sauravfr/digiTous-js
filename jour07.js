d2f55d746435a6edb8a5f76f4fee2c0bf76fdf36
Emma Paris <eparis@atlassian.com>
2020-12-18 11:03:47 +0100

    Add 03-Palindrome

--- jour07.js
+++ jour07.js
@@ -14,6 +14,18 @@ function countEach(string) {
 }
 countEach("xxxoooox");
 
+//03 - Palindrome
+var checkPal = function(string) {
+    var str = string.split('').reverse().join('')
+    if (string = str) {
+        return "Palindrome !";
+    } else {
+        return "Nope";
+    }
+}
+var check = checkPal("racecar");
+console.log(check);
+
 //04 - Swap
 
 function swap(text) {
--- test2.js
+++ test2.js
@@ -1,13 +1,11 @@
-//Bonus II
-
-function caseInsensitiveSort(strA, strB) {
-    return strA.toLowerCase().localeCompare(strB.toLowerCase());
+//03 - Palindrome
+var checkPal = function(string) {
+    var str = string.split('').reverse().join('')
+    if (string = str) {
+        return "Palindrome !";
+    } else {
+        return "Nope";
+    }
 }
-
-function sortLetters(text) {
-    text = text.split("");
-    text = text.sort(caseInsensitiveSort);
-    text = text.join("");
-    console.log(text);
-}
-sortLetters("koNeXio");
+var check = checkPal("racecar");
+console.log(check);
