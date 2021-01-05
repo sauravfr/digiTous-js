8b21c5c2180643faf871016628ae8079f0c680d5
Emma Paris <eparis@atlassian.com>
2021-01-05 14:47:44 +0100

    add jour08-js file

---
+++ jour08.js
@@ -0,0 +1,13 @@
+var request = require('request');
+//01 - Countries
+
+/*var countriesNames = [];
+
+
+
+request.get("https://restcountries.eu/rest/v1/all", function(err, response, body) {
+    console.log(err);
+    var coutriesNames = JSON.parse(body)
+    for (i = 0; i < coutriesNames.length; i++)
+        console.log(coutriesNames[i].name);
+});*/
