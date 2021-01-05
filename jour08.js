afae00e436de0d8035c76c27339f5ce1617ef7f2
Emma Paris <eparis@atlassian.com>
2021-01-05 16:13:51 +0100

    add 02-Chuck Norris

--- jour08.js
+++ jour08.js
@@ -11,3 +11,14 @@ function getCountries() {
     });
 }
 getCountries();
+
+// 02 - Chuck Norris
+
+function getFact() {
+    request.get("https://api.chucknorris.io/jokes/random", function(err, response, body) {
+        console.log(err);
+        var value = JSON.parse(body).value;
+        console.log(value);
+    });
+}
+getFact();
--- test.js
+++ test.js
@@ -1,9 +1,13 @@
+var request = require('request');
+
+
 // 02 - Chuck Norris
+
 function getFact() {
-    request.get("https://api.chucknorris.io/jokes/random", function(err, res, body) {
-        var joke = JSON.parse(body);
-        var value = joke.value;
+    request.get("https://api.chucknorris.io/jokes/random", function(err, response, body) {
+        console.log(err);
+        var value = JSON.parse(body).value;
         console.log(value);
-    })
+    });
 }
-getFact()
+getFact();
