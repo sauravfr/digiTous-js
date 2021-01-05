6f4027bdb2533aa5e37c7fca561d84087d6b0e6f
Emma Paris <eparis@atlassian.com>
2021-01-05 15:59:47 +0100

    Add 01-Countries

--- jour08.js
+++ jour08.js
@@ -1,13 +1,13 @@
 var request = require('request');
 //01 - Countries
+var countriesNames = [];
 
-/*var countriesNames = [];
-
-
-
-request.get("https://restcountries.eu/rest/v1/all", function(err, response, body) {
-    console.log(err);
-    var coutriesNames = JSON.parse(body)
-    for (i = 0; i < coutriesNames.length; i++)
-        console.log(coutriesNames[i].name);
-});*/
+function getCountries() {
+    request.get("https://restcountries.eu/rest/v1/all", function(err, response, body) {
+        console.log(err);
+        coutriesNames = JSON.parse(body)
+        for (i = 0; i < coutriesNames.length; i++)
+            console.log(coutriesNames[i].name);
+    });
+}
+getCountries();
--- test.js
+++ test.js
@@ -1,40 +1,9 @@
-//06 - Cakes
-var cakes = [{
-        name: "cake",
-        flavor: "vanilla",
-        status: "available"
-    },
-    {
-        name: "brownie",
-        flavor: "chocolate",
-        status: "available"
-    },
-    {
-        name: "pie",
-        flavor: "strawberry",
-        status: "available"
-    },
-    {
-        name: "muffin",
-        flavor: "pistachio",
-        status: "available"
-    },
-    {
-        name: "donut",
-        flavor: "chocolate",
-        status: "available"
-    },
-];
-
-var soldOut = cakes.filter(cake => {
-    if (cake.flavor == "chocolate") {
-        return true;
-    }
-}).map(cake => {
-    if (cake.flavor === "chocolate") {
-        cake.status = "sold out!"
-        return cake
-    }
-
-})
-console.log(soldOut);
+// 02 - Chuck Norris
+function getFact() {
+    request.get("https://api.chucknorris.io/jokes/random", function(err, res, body) {
+        var joke = JSON.parse(body);
+        var value = joke.value;
+        console.log(value);
+    })
+}
+getFact()
